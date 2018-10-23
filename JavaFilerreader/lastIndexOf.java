4ed30f38143f1427b7c6b9c4e36fffd2f2166f5f
shaunieboi92 <shau0004@e.ntu.edu.sg>
2018-10-23 14:19:24 +0800

    Add files via upload

---
+++ JavaFilerreader/lastIndexOf.java
@@ -0,0 +1,8 @@
+
+public class lastIndexOf {
+	public static void main (String args []) {
+		String name = "Shaun.com";
+		System.out.println(name.lastIndexOf("."));
+		//this will essentially print out the number of characters before the character in question
+	}
+}
